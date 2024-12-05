5654833d655a13959786278a8bcc9bffada0698c
dimitris986 <162153283+dimitris986@users.noreply.github.com>
2024-12-05 10:37:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def load_data_from_api(user_id):
 
 # Δημιουργία του Dash app
 app = dash.Dash(__name__, suppress_callback_exceptions=True)
-#server = app.server
+server = app.server
 
 app.layout = html.Div([
     dcc.Location(id="url", refresh=False),
